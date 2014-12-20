54d54f95827ac1f32cc7970beaa61551ef08c95f
Raman Prasad <raman_prasad@harvard.edu>
2014-12-20 06:23:31 -0500

    update to formatting, 1st time use

--- shared_dataverse_information/map_layer_metadata/forms.py
+++ shared_dataverse_information/map_layer_metadata/forms.py
@@ -17,19 +17,31 @@ class MapLayerMetadataValidationForm(forms.ModelForm):
                # , 'name': forms.TextInput(attrs={'size':20})
                 }
 
-    def format_data_for_dataverse_api(self, session_token_value):
+    def format_data_for_dataverse_api(self, session_token_value=None):
         """
         Format the key names to make them compatible with the dataverse API
 
-        :param session_token_value:
-        :return:
+        :param session_token_value: if not specified, use the form's dv_session_token value
+        :return: dict with formatted parameters
         """
         global KEY_MAPPING_FOR_DATAVERSE_API, DATAVERSE_REQUIRED_KEYS
 
         assert self.cleaned_data is not None, "cleaned_data not found.  Call and verify that form is_valid()"
+        
+        # If the session_token_value is not specified, 
+        #   check for one in cleaned_data
+        #
+        # For the model/form, the session_token_value is optional
+        #   
+        if session_token_value is None:
+            session_token_value = self.cleaned_data.get('dv_session_token')
+                        
         assert session_token_value is not None, "A session token is required"
+        assert session_token_value is not '', "A session token is required"
+        print('dataversekeys: %s' % set(DATAVERSE_REQUIRED_KEYS))
+        print('form keys: %s' %self.cleaned_data.keys())
 
-        missing_required_keys = list(set(DATAVERSE_REQUIRED_KEYS.keys()) - set(self.cleaned_data.keys()))
+        missing_required_keys = list(set(KEY_MAPPING_FOR_DATAVERSE_API.keys()) - set(self.cleaned_data.keys()))
 
         assert len(missing_required_keys) == 0, "Not all required keys found in form.  Required keys not found: %s" % missing_required_keys
 
@@ -40,7 +52,7 @@ class MapLayerMetadataValidationForm(forms.ModelForm):
             # make sure all the keys are there!
             assert worldmap_key is not None, "Key in MapLayerMetadata model not found in KEY_MAPPING_FOR_DATAVERSE_API api: %s" % k
 
-            formatted_dict['worldmap_key'] = v
+            formatted_dict[worldmap_key] = v
 
         return formatted_dict
 
