707f373222848492a3945ff7cd3e74fe27543118
Raman Prasad <raman_prasad@harvard.edu>
2014-12-22 13:44:36 -0500

    Add map_image_link field to MapLayerMetadata object

---
+++ HISTORY.rst
@@ -0,0 +1,12 @@
+.. :changelog:
+
+Release History
+---------------
+
+0.2.4 (2014-12-22)
+++++++++++++++++++
+
+**Updates**
+
+- Add map_image_link field to MapLayerMetadata object
+- added index to DataverseInfo.datafile_id
--- shared_dataverse_information/map_layer_metadata/models.py
+++ shared_dataverse_information/map_layer_metadata/models.py
@@ -7,6 +7,7 @@ KEY_MAPPING_FOR_DATAVERSE_API = { 'worldmap_username' : 'worldmapUsername'\
                                     , 'embed_map_link' : 'embedMapLink'\
                                     , 'datafile_id': 'datafileID'\
                                     , 'dv_session_token' : settings.DATAVERSE_TOKEN_KEYNAME\
+                                    , 'map_image_link' : 'mapImageLink'\
                                     }
 DATAVERSE_REQUIRED_KEYS = KEY_MAPPING_FOR_DATAVERSE_API.values()
 
@@ -22,12 +23,13 @@ class MapLayerMetadata(models.Model):
     embed_map_link = models.URLField(max_length=255)
     datafile_id = models.IntegerField(default=-1, help_text='id in database')
 
+    map_image_link = models.URLField(max_length=255)
     dv_session_token = models.CharField(max_length=255, blank=True)
 
 
     def __unicode__(self):
         return '%s (file: %s; user: %s )' % (self.layer_name, self.datafile_id, self.worldmap_username)
-
+        
     class Meta:
         abstract = True
         verbose_name_plural = 'Map Layer Metadata'
