887972c7feb46507d4071d499f05749aa54510d1
Raman Prasad <raman_prasad@harvard.edu>
2015-02-04 14:51:44 -0500

    fix for ClassifyRequestDataForm

--- shared_dataverse_information/layer_classification/forms_api.py
+++ shared_dataverse_information/layer_classification/forms_api.py
@@ -10,6 +10,17 @@ class ClassifyRequestDataForm(APIValidateHelperForm):
     def get_validation_field_names(self):
         return ('layer_name', 'attribute', 'method')
 
+    def clean_layer_name(self):
+        """
+        "geonode:my_layer_name" becomes "my_layer_name"
+        "my_layer_name" stays "my_layer_name"
+        """
+        layer_name = self.cleaned_data.get('layer_name', None)
+        if layer_name is None:
+            raise forms.ValidationError(_('The layer name must be specified'), code='invalid')
+
+        return layer_name.split(':')[-1]
+            
     class Meta:
         model = ClassifyRequestData
 
